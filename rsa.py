aa8e918820a0faa9db20fe2dbcbf5ca621b9fed0
Alejandro <gar231136@uvg.egu.gt>
2024-11-17 22:21:22 -0600

    MCD

---
+++ rsa.py
@@ -0,0 +1,61 @@
+'''
+Universidad del Valle de Guatemala
+Matemática Discreta
+Ing. Mario Castillo
+'''
+
+
+def mcd(a, b):
+    r = a % b
+    while r!=0:
+        a = b
+        b = r
+        r = a % b 
+    return b
+print(" ------------------------------------------------")
+print(" |                    MCD                       |")
+print(" ------------------------------------------------")
+a = int(input("Ingrese un entero positivo a: "))
+b = int(input("Ingrese un entero positivo b: "))
+print("El mdc de", a, "y", b, "es", mcd(a, b))
+
+
+# Función que lee un entero positivo y valida que sea mayor que 0
+def validacion(num):
+    while True:
+        if num.isdigit():  
+            num = int(num)
+            if num >=2:
+                return num
+            else:
+                print(" * Ingrese un entero positivo.")
+        else:
+            print(" * Entrada inválida. Debe ingresar un número entero mayor o igual a 2.")
+        
+        num = input("\n - Ingrese nuevamente un número: ")
+
+
+def Criba(n):
+    # Lista de primos
+    primes = []
+    # LIstas de no primos
+    not_primes = set()
+    # Recorre desde dos hasta el número aumentado en uno
+    for i in range(2,n+1):
+
+        # Verifica si i está en no primos, si ya está salta el índice
+        if i in not_primes:
+            continue
+        # Verifica los múltiplos de i y los agrega en la lista de no primos de j hasta n
+        for j in range(i*2,n+1,i):
+            not_primes.add(j)
+        # Después de las validaciones añade a i al array de primos
+        primes.append(i)
+    return primes
+
+# Punto de inico del programa
+print(" ------------------------------------------------")
+print(" |            CRIBA DE ERATÓSTENES             |")
+print(" ------------------------------------------------")
+num = validacion(input(" - Introduzca un número: "))
+print(" - Los primos menores o iguales a 28 son:", Criba(int(num)))
